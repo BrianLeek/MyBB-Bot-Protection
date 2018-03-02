78872f42959ad1a9c54d50afed2ea3e72e32824f
Brian <31559308+BrianLeek@users.noreply.github.com>
2018-03-02 15:03:48 +0000

    Created forum_botprotection.lang.php

---
+++ inc/languages/english/admin/forum_botprotection.lang.php
@@ -0,0 +1,14 @@
+<?php
+#########################################################
+# English language pack                                 #
+# Version 1.0                                           #
+# MyBB-Version 1.8.                                      #
+# Autor: Brian.                                #
+# Website: https://community.mybb.com/user-115119.html                       #
+#                                                       #
+#########################################################
+
+$l['botprotection_title'] = "Bot Protection";
+$l['botprotection_desc'] = "Plugin, that prohibits bots from registering using a hidden field that filled by Javascript. As long as bots don't parse Javascript, this can be used as a replacement for captchas.";
+
+?>
