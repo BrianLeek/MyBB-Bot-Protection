5926e513394accf1f6fc448e676ae160c9ad3fdb
Brian <31559308+BrianLeek@users.noreply.github.com>
2018-03-02 15:02:43 +0000

    Created botprotection.lang.php

---
+++ inc/languages/english/botprotection.lang.php
@@ -0,0 +1,14 @@
+<?php
+#########################################################
+# English language pack                                 #
+# Version 1.0                                           #
+# MyBB-Version 1.8                                      #
+# Autor: Brian.                                 #
+# Website: https://community.mybb.com/user-115119.html                      #
+#                                                       #
+#########################################################
+
+$l['botprotection_error_jsdisabled'] = "Javascript is not active, please activate Javascript for registration!";
+$l['botprotection_error_jsdisabled_title'] = "Javascript";
+
+?>
